6e64a7ae45a91423d1ee8b3b6598854251cf30ae
Yousef Montash <yousefmontash2002@gmail.com>
2022-09-05 09:09:04 +0200

    Task 02: Tests Passed

---
+++ test.py
@@ -0,0 +1,14 @@
+from sprints import MyFunc
+lst1 = ["20","30","31","22.1","21.3","hghgf","36.14"]
+assert MyFunc(lst1) == "25.0 , 36.14" , "25 & 36.14"
+lst2 = ["car","game"]
+assert MyFunc(lst2) == "0"
+
+
+
+
+
+
+
+
+
